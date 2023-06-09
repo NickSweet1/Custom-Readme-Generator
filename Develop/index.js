67706c5a211cd40d2b8014e8f630ee855323d3ed
Nick <nicksweetj@gmail.com>
2023-06-08 22:54:56 -0400

    finished markdown.js functionality? May need some review. Also added layout for the questions to be asked and to write the readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,39 @@
-// TODO: Include packages needed for this application
-
+// TODO: Include packages needed for this application i
+const fs = require("fs");
+const generateMarkdown = require('./utils/generateMarkdown');
+const { error } = require("console");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "Title",
+        question: "What is the title of the project that you are creating this ReadMe file for?"
+    },
+    {
+        name: "License Type",
+        question: "What license would you prefer to be used for your personalized ReadMe file?"
+    },
+    {
+        name: "Installation",
+        question: "Please breifly describe any installations needed to use your application."
+    }
+//needs the rest of the questions logged ************************************************************************
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        if (err) {
+            console.log("error");
+        } else {
+            console.log("ReadMe file created.");
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge = "";
+
+  switch (license) {
+    case "MIT": 
+    licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+    break;
+    case "Apache 2.0":
+    licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]" ;
+    break;
+    case "GPLv3":
+    licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+    break;
+    default: 
+    return "";
+  }
+
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = "";
+
+  switch (license) {
+    case "MIT": 
+    licenseLink = "[License: MIT](https://opensource.org/licenses/MIT)";
+    break;
+    case "Apache":
+    licenseLink = "[License](https://opensource.org/licenses/Apache-2.0)" ;
+    break;
+    case "GNU":
+    licenseLink = "[License: GPL v3](https://www.gnu.org/licenses/gpl-3.0)";
+    break;
+    default: 
+    return "";
+  }
+
+  return licenseLink;
+}
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `This project is licensed under ${license}. \n ${renderLicenseBadge(license)}, ${renderLicenseLink(license)}.`
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `
+  # ${data.title}
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+`; //needs the rest of the imformation logged ********************************************
 }
 
 module.exports = generateMarkdown;
